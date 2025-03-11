a2b5b4f25759b6683947d6a8edc7ffd95c305ab4
DragonCoder283 <roblox.player283382@gmail.com>
2025-03-10 20:59:45 -0400

    add stuff

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import gspread
 import statbotics
 import json
 import requests
@@ -25,11 +24,6 @@ auth_key.close()
 stats = statbotics.Statbotics()
 TBA_AUTH_KEY = key["key"]
 TBA_BASE_ENDPOINT = "https://www.thebluealliance.com/api/v3"
-
-gc = gspread.service_account(filename="json-files/credentials.json")
-
-# Open a sheet from a spreadsheet in one go
-sheet = gc.open(spreadsheet_name).sheet1
 
 event_keys = [
     "2025miber"
@@ -68,90 +62,6 @@ def update_event_json():
         with open(f"./json-files/{event}.json", "w+") as jsonFile:
             data = call_tba_api(f"/event/{event}/teams")
             jsonFile.write(json.dumps(data))
-
-
-async def update_sheet():
-    # update json files for entered event keys
-    clear_vars()
-    update_event_json()
-    # create lists that will store gathered data so that we can push all the data in one go
-
-    # store team numbers, names, and state/provinces
-    for event in event_keys:
-        with open(f"./json-files/{event}.json", "r") as data:
-            data = json.load(data)
-            for item in data:
-                try:
-                    team_numbers.append([item["team_number"]])
-                except Exception:
-                    team_numbers.append(["N/A"])
-                try:
-                    team_names.append([item["nickname"]])
-                except Exception:
-                    team_names.append(["N/A"])
-                try:
-                    team_states.append([item["state_prov"]])
-                except Exception:
-                    team_states.append(["N/A"])
-
-    # store team epas, winrates, and rookie years
-    for team in team_numbers:
-        stat_by_team.clear()
-        stat_by_team.append(stats.get_team_year(team[0], 2025))
-
-        auto_move.append([auto_leave(team[0])])
-        climb.append([can_climb(team[0])])
-        team_epas.append([stat_by_team[0]["epa"]["breakdown"]["total_points"]])
-        rookie_years.append([stats.get_team(team[0])["rookie_year"]])
-        team_winrate = round(stat_by_team[0]["record"]["winrate"] * 100, 1)
-        team_winrates.append([f"{team_winrate}%"])
-        events_played.append([team_events_played(team[0])])
-
-    # use batch update to avoid google requests rate limit
-    sheet.batch_update([{
-        "range": f"{team_number_column}2:{team_number_column}{len(team_numbers) + 1}",
-        "values": team_numbers
-    }])
-
-    sheet.batch_update([{
-        "range": f"{team_name_column}2:{team_name_column}{len(team_names) + 1}",
-        "values": team_names
-    }])
-
-    sheet.batch_update([{
-        "range": f"{team_state_column}2:{team_state_column}{len(team_states) + 1}",
-        "values": team_states
-    }])
-
-    sheet.batch_update([{
-        "range": f"{team_epa_column}2:{team_epa_column}{len(team_epas) + 1}",
-        "values": team_epas
-    }])
-
-    sheet.batch_update([{
-        "range": f"{team_winrate_column}2:{team_winrate_column}{len(team_winrates) + 1}",
-        "values": team_winrates
-    }])
-
-    sheet.batch_update([{
-        "range": f"{team_rookie_year_column}2:{team_rookie_year_column}{len(rookie_years) + 1}",
-        "values": rookie_years
-    }])
-
-    sheet.batch_update([{
-        "range": f"{team_auto_move_column}2:{team_auto_move_column}{len(auto_move) + 1}",
-        "values": auto_move
-    }])
-
-    sheet.batch_update([{
-        "range": f"{team_climb_column}2:{team_climb_column}{len(climb) + 1}",
-        "values": climb
-    }])
-
-    sheet.batch_update([{
-        "range": f"{team_events_played_column}2:{team_events_played_column}{len(climb) + 1}",
-        "values": events_played
-    }])
 
 def get_epa_by_team(team, epa_type):
     try:
@@ -247,6 +157,112 @@ def auto_leave(team_num):
             j += 1
     return False
 
+def climb_percentage(team_num):
+    global match
+    team_slot = 0
+    red = False
+    i = 1
+    j = 0
+    climbs = 0
+    keys = []
+
+    events_climb = call_tba_api(f"team/frc{team_num}/events/2025")
+    try:
+        if "does not exist" in events_climb["Error"]:
+            return "0"
+    except Exception:
+        pass
+
+    for event in events_climb:
+        keys.append(event["key"])
+
+        while j < len(keys):
+            matches = call_tba_api(f"team/frc{team_num}/event/{keys[j]}/matches")
+            if not matches:
+                break
+            match = 0
+            while match < len(matches):
+                for team in matches[match]["alliances"]["blue"]["team_keys"]:
+                    if team == f"frc{team_num}":
+                        team_slot = i
+                        red = False
+                    i += 1
+                i = 1
+                for team in matches[match]["alliances"]["red"]["team_keys"]:
+                    if team == f"frc{team_num}":
+                        team_slot = i
+                        red = True
+                    i += 1
+                i = 1
+
+                if red is False:
+                    climb_ = call_tba_api(f"team/frc{team_num}/event/{keys[j]}/matches")[match]["score_breakdown"]["blue"][
+                        f"endGameRobot{team_slot}"]
+                else:
+                    climb_ = call_tba_api(f"team/frc{team_num}/event/{keys[j]}/matches")[match]["score_breakdown"]["red"][
+                        f"endGameRobot{team_slot}"]
+                match = match + 1
+
+                # save all results in list, check if list contains one by one, and if contains none, return N/A
+                if climb_ == "DeepCage" or climb_ == "ShallowCage":
+                    climbs += 1
+            j += 1
+
+    return f'{round((climbs/match)*100, 1)}'
+
+def auto_leave_percentage(team_num):
+    global match
+    team_slot = 0
+    red = False
+    i = 1
+    j = 0
+    leaves = 0
+    keys = []
+
+    events_climb = call_tba_api(f"team/frc{team_num}/events/2025")
+    try:
+        if "does not exist" in events_climb["Error"]:
+            return "0"
+    except Exception:
+        pass
+
+    for event in events_climb:
+        keys.append(event["key"])
+
+        while j < len(keys):
+            matches = call_tba_api(f"team/frc{team_num}/event/{keys[j]}/matches")
+            if not matches:
+                break
+            match = 0
+            while match < len(matches):
+                for team in matches[match]["alliances"]["blue"]["team_keys"]:
+                    if team == f"frc{team_num}":
+                        team_slot = i
+                        red = False
+                    i += 1
+                i = 1
+                for team in matches[match]["alliances"]["red"]["team_keys"]:
+                    if team == f"frc{team_num}":
+                        team_slot = i
+                        red = True
+                    i += 1
+                i = 1
+
+                if red is False:
+                    leave_ = call_tba_api(f"team/frc{team_num}/event/{keys[j]}/matches")[match]["score_breakdown"]["blue"][
+                        f"autoLineRobot{team_slot}"]
+                else:
+                    leave_ = call_tba_api(f"team/frc{team_num}/event/{keys[j]}/matches")[match]["score_breakdown"]["blue"][
+                        f"autoLineRobot{team_slot}"]
+                match = match + 1
+
+                # save all results in list, check if list contains one by one, and if contains none, return N/A
+                if leave_:
+                    leaves += 1
+            j += 1
+
+    return f'{round((leaves/match)*100, 1)}'
+
 def can_climb(team_num):
     team_slot = 0
     red = False
@@ -272,13 +288,13 @@ def can_climb(team_num):
                     if team == f"frc{team_num}":
                         team_slot = i
                         red = False
-                    i = i + 1
+                    i += 1
                 i = 1
                 for team in matches[match]["alliances"]["red"]["team_keys"]:
                     if team == f"frc{team_num}":
                         team_slot = i
                         red = True
-                    i = i + 1
+                    i += 1
                 i = 1
 
                 if red is False:
